84ef407febe2cde25fd4cadb686007f315b044b7
loris.perret <94396192+LorisPERRET@users.noreply.github.com>
2026-07-06 12:18:28 +0200

    Upgrade iOS16 and swift 6.2 (#17)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftiumKit",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v16)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
